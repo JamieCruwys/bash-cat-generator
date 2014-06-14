1c3a8b8d4b97118b11169cbb552e1c3bb35f4369
Jamie Cruwys <me@jamiecruwys.co.uk>
2014-06-14 17:12:05 +0100

    Update cats.sh
    Fixed function brackets

--- cats.sh
+++ cats.sh
@@ -3,7 +3,8 @@ source menu.sh
 
 declare -a options=("Hell Yeah!" "No thanks");
 
-function getCats() {
+function getCats() 
+{
 	xml=$(curl -s 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1&type=jpg' -o outfile.xml)
 	file=$(sed -n -e 's/.*<url>\(.*\)<\/url>.*/\1/p' outfile.xml)
 	curl -s $file -o cat.jpg
