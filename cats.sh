26bbde8daf0e05020ff3d17eb9b2ae3bb7df0205
Jamie Cruwys <me@jamiecruwys.co.uk>
2014-06-14 17:11:07 +0100

    Added cat bash script

---
+++ cats.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source menu.sh
+
+declare -a options=("Hell Yeah!" "No thanks");
+
+function getCats() {
+	xml=$(curl -s 'http://thecatapi.com/api/images/get?format=xml&results_per_page=1&type=jpg' -o outfile.xml)
+	file=$(sed -n -e 's/.*<url>\(.*\)<\/url>.*/\1/p' outfile.xml)
+	curl -s $file -o cat.jpg
+	rm outfile.xml
+	open cat.jpg
+	askForCats
+}
+
+function askForCats()
+{
+	clear
+	generateDialog "options" "Would you like to see cats?" "${options[@]}"
+
+	read choice
+	case $choice in
+		1) getCats; ;;
+		2) rm cat.jpg&>- exit 0;;
+		*) askForCats ;;
+	esac
+}
+
+askForCats;
